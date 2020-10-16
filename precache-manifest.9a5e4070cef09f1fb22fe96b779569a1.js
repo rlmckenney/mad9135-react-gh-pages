88a626b59ceda25ae7e3a65f3768697d34c46f59
Robert McKenney <robert@mckenney.ca>
2020-10-16 11:11:53 -0400

    Updates

---
+++ precache-manifest.9a5e4070cef09f1fb22fe96b779569a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c9775c1600ecae572504d499d55117",
+    "url": "/mad9135-react-gh-pages/index.html"
+  },
+  {
+    "revision": "a711b5713f0941974427",
+    "url": "/mad9135-react-gh-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e02d4eb2dc7f1c410f4f",
+    "url": "/mad9135-react-gh-pages/static/js/2.853f36e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mad9135-react-gh-pages/static/js/2.853f36e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a711b5713f0941974427",
+    "url": "/mad9135-react-gh-pages/static/js/main.5d433ca5.chunk.js"
+  },
+  {
+    "revision": "bad542741ee338daf007",
+    "url": "/mad9135-react-gh-pages/static/js/runtime-main.2b2739ad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rlmckenney/mad9135-react-gh-pages/precache-manifest.c7bba8f810c190669d59dbcf17b84787.js"
+  "/mad9135-react-gh-pages/precache-manifest.9a5e4070cef09f1fb22fe96b779569a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rlmckenney/mad9135-react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mad9135-react-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
