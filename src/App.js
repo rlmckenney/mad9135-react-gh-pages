0a436ef1907f01c34a6b4b33b7794dc67f03f720
Robert McKenney <robert@mckenney.ca>
2020-10-16 11:09:24 -0400

    Add configuration for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-function App() {
+const Home = () => <h1>Home</h1>
+const About = () => <h1>About</h1>
+const Contact = () => <h1>Contact</h1>
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <header>
+          <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
